c0f48d71bffd6620548c51c232754949eae9ded0
VazardAU <vazard@hotmail.com>
2023-07-12 16:46:04 +1000

    minio tf

---
+++ terraform/s3/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    storj = {
+      source = "storj/storj"
+      version = "0.0.1"
+    }
+
+    minio = {
+      source = "aminueza/minio"
+      version = "1.15.2"
+    }
+  }
+}
